30e67776823f8867770dc3a920d74ea502b74934
Nickeron-dev <koshkin061969@gmail.com>
2021-06-21 14:03:39 +0300

    Added design for all laptops, computers, cameras, monowheels and a few scooters.

--- Shop.zip_expanded/Shop/src/main/java/com/shop/Shop/ShopController.java
+++ Shop.zip_expanded/Shop/src/main/java/com/shop/Shop/ShopController.java
@@ -127,4 +127,68 @@ public class ShopController {
 	public String dellXPS15() {
 		return "dellXPS15";
 	}
+	
+	// Computers
+	@GetMapping("computers/mac-mini-m1")
+	public String macMiniM1() {
+		return "macMiniM1";
+	}
+	
+	@GetMapping("computers/mac2020")
+	public String mac2020() {
+		return "mac2020";
+	}
+	
+	@GetMapping("computers/macM1")
+	public String macM1() {
+		return "macM1";
+	}
+	
+	// Cameras
+	@GetMapping("/cameras/djiPocket2")
+	public String djiPocket2() {
+		return "djiPocket2";
+	}
+	
+	@GetMapping("/cameras/xiaomiSeabird")
+	public String xiaomiSeabird() {
+		return "xiaomiSeabird";
+	}
+	
+	@GetMapping("/cameras/goPro9")
+	public String goPro9() {
+		return "goPro9";
+	}
+	
+	// Monowheels
+	@GetMapping("monowheels/inmotionV8")
+	public String inmotionV8() {
+		return "inmotionV8";
+	}
+	
+	@GetMapping("monowheels/inmotionV10F")
+	public String inmotionV10F() {
+		return "inmotionV10F";
+	}
+	
+	@GetMapping("monowheels/ninebotZ10")
+	public String ninebotZ10() {
+		return "ninebotZ10";
+	}
+	
+	// Scooters
+	@GetMapping("scooters/scooter1s")
+	public String scooter1s() {
+		return "scooter1s";
+	}
+	
+	@GetMapping("scooters/ninebotE10")
+	public String ninebotE10() {
+		return "ninebotE10";
+	}
+	
+	@GetMapping("scooters/ninebotE45E")
+	public String ninebotE45E() {
+		return "ninebotE45E";
+	}
 }
